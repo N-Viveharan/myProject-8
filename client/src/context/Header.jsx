bbe409306b1f799962dd3ec5dc335b2fec51fbb6
N-Viveharan <nagalingamviveharan2001@gmail.com>
2026-03-10 21:45:43 +0530

    edit the header

--- client/src/context/Header.jsx
+++ client/src/context/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div>
+      <div>
+        <h1>Remove the <br /><span>from</span> <br />images for free</h1>
+        <p>lorem Ipsum is simply dum scem lesum has been the
+          out of the printing and typesetting industry: stry's standard dummy text</p>
+          <div>
+            <input type="file" name="" id="" />
+            <label htmlFor="">
+              <img src={assets.upload_btn_icon} alt="" />
+
+            </label>
+          </div>
+      </div>
+    </div>
   )
 }
 
 export default Header
+
+
