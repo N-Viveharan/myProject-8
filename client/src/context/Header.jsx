c563ae3351843215e83f7cbe753243ce0731ab96
N-Viveharan <nagalingamviveharan2001@gmail.com>
2026-03-10 21:50:33 +0530

    edit the headers

--- client/src/context/Header.jsx
+++ client/src/context/Header.jsx
@@ -3,15 +3,16 @@ import { assets } from '../assets/assets'
 
 function Header() {
   return (
-    <div>
-      <div>
+    <div className='flex items-center justify-between max-sm:flex-col-reverse gap-y-10 px-4 mt-10 lg:px-44 sm:mt-20'>
+      <div >
         <h1>Remove the <br /><span>from</span> <br />images for free</h1>
         <p>lorem Ipsum is simply dum scem lesum has been the
           out of the printing and typesetting industry: stry's standard dummy text</p>
           <div>
-            <input type="file" name="" id="" />
-            <label htmlFor="">
+            <input type="file" name="" id="" hidden />
+            <label htmlFor="upload1">
               <img src={assets.upload_btn_icon} alt="" />
+              <p>Update your image</p>
 
             </label>
           </div>
