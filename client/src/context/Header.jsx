63272bda3c0a7ebebb1a3dfd692279765ac2ab3e
N-Viveharan <nagalingamviveharan2001@gmail.com>
2026-03-10 21:55:15 +0530

    edits the headers

--- client/src/context/Header.jsx
+++ client/src/context/Header.jsx
@@ -5,7 +5,8 @@ function Header() {
   return (
     <div className='flex items-center justify-between max-sm:flex-col-reverse gap-y-10 px-4 mt-10 lg:px-44 sm:mt-20'>
       <div >
-        <h1>Remove the <br /><span>from</span> <br />images for free</h1>
+        <h1 className='text-4xl xl:text-6xl font-bold text-neutral-700 leading-tight'>
+          Remove the <br /><span className='bg-gradient-to-r from-violet-600 to-fuchsia-500 bg-clip-text text-transparent'>from</span> <br />images for free</h1>
         <p>lorem Ipsum is simply dum scem lesum has been the
           out of the printing and typesetting industry: stry's standard dummy text</p>
           <div>
