9907d030ec880ae57d2550f0bf508858349ff16b
N-Viveharan <nagalingamviveharan2001@gmail.com>
2026-03-13 22:14:43 +0530

    edit the bgslide

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../context/Header'
 import Steps from '../context/Steps'
+import BgSlider from '../context/BgSlider'
 
 function Home() {
   return (
     <div>
       <Header/>
       <Steps/>
+      <BgSlider/>
     </div>
   )
 }
