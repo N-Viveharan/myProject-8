de76099605ffdbfcbe4ce531c0572f5b7f07eff8
N-Viveharan <nagalingamviveharan2001@gmail.com>
2026-03-13 21:17:32 +0530

    create the step

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../context/Header'
+import Steps from '../context/Steps'
 
 function Home() {
   return (
     <div>
       <Header/>
+      <Steps/>
     </div>
   )
 }
