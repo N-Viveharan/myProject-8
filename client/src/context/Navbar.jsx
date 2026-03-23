9f721b01c3c44ac0066f7fde6f2690b3871e18f8
N-Viveharan <nagalingamviveharan2001@gmail.com>
2026-03-24 00:32:31 +0530

    edit the navbar

--- client/src/context/Navbar.jsx
+++ client/src/context/Navbar.jsx
@@ -5,6 +5,8 @@ function Navbar() {
   return (
     <div className='flex items-center justify-between mx-4 py-3  lg:mx-44'>
        <Link to='/'> <img  className='w-32 sm:w-44' src={assets.logo} alt="" /></Link>
+
+       
         <button className='bg-zinc-800 text-white flex item gap-4 px-4 py-2 sm:px-8 sm:py-3 text-sm rounded-full'>Get Started <img className='w-3 sm:w-4' src={assets.arrow_icon} alt="" /></button>
         
     </div>
