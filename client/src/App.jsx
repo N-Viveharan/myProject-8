a8c1dd696fd71f4da902d9e248d08f40d0575de0
N-Viveharan <nagalingamviveharan2001@gmail.com>
2026-03-25 21:55:26 +0530

    edit  result

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,6 +18,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/result" element={<Result />} />
         <Route path="/buy" element={<BuyCredit />} />
+        
       </Routes>
       <Footer/>
     </div>
