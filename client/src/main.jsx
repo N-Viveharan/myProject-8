f5cd3da1fef718651f6cedb66212fabe830bd34c
N-Viveharan <nagalingamviveharan2001@gmail.com>
2026-03-27 19:29:52 +0530

    main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 
 import App from './App.jsx'
+
 import { ClerkProvider } from '@clerk/react'
 import { BrowserRouter } from "react-router-dom";
 
